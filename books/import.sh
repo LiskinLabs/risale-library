24b0d7e55ff4d27a7df64fbfc20679e9f1f67379
Silvestr Liskin <silvestr.liskin@teknorob.merkez>
2026-04-24 17:25:38 +0300

    apply jules migration changes

---
+++ books/import.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Placeholder script for importing books into SQLite database
+echo "Importing books..."
+# TODO: Implement actual logic
---
+++ lugat/import.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Placeholder script for importing lugat into SQLite database
+echo "Importing lugat..."
+# TODO: Implement actual logic
